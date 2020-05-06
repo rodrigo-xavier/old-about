774b97f3643fba963905ceac0a42fd716f621e63
rodrigo-xavier <rodrigomatiasxavier@gmail.com>
2020-05-06 07:27:43 -0300

    making tests

--- tests/test_utils/test_models.py
+++ tests/test_utils/test_models.py
@@ -4,23 +4,111 @@ from unittest import mock
 import pytest
 
 
-class PhoneFieldTestCase(TestCase):
-    def setUp(self):
-        PhoneField.objects.create(phone=9999)
-        PhoneField.objects.create(phone=999999999)
-        PhoneField.objects.create(phone=000000000)
-        PhoneField.objects.create(phone=999999999999999)
-        # PhoneField.objects.create(phone=+00000000000000)
-    
-    def test_validate_phone(self):
-        phone = PhoneField.objects.all()
+# Tips:
+# refresh_from_db() if you delete or update a field from a model instance
+# validators are caled with clean methods
 
-        assert phone[0].phone == '9999'
-        assert phone[1].phone == '999999999'
-        assert phone[2].phone == '0'
-        assert phone[3].phone == '999999999999999'
-        # assert phone[4].phone != 9999999999999999
-        
-    # def tearDown(self):
-    #     PhoneField.objects.get.all().delete()
-    #     return super().tearDown()
+
+class PhoneFieldTestCase(TestCase):
+
+    def test_validate_phone_1(self):
+        p = PhoneField.objects.create(phone="999999999")
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '999999999'
+
+    def test_validate_phone_2(self):
+        p = PhoneField.objects.create(phone="000000000")
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '000000000'
+    
+    def test_validate_phone_3(self):
+        p = PhoneField.objects.create(phone="9999999999999")
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '9999999999999'
+
+    def test_validate_phone_4(self):
+        p = PhoneField.objects.create(phone="0000000000000")
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '0000000000000'
+
+    def test_validate_phone_5(self):
+        p = PhoneField.objects.create(phone=999999999)
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '999999999'
+
+    @pytest.mark.xfail
+    def test_validate_phone_6(self):
+        p = PhoneField.objects.create(phone=000000000) # int(000000000) = str(0)
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '000000000'
+    
+    def test_validate_phone_7(self):
+        p = PhoneField.objects.create(phone=9999999999999)
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '9999999999999'
+
+    @pytest.mark.xfail
+    def test_validate_phone_8(self):
+        p = PhoneField.objects.create(phone=0000000000000) # int(0000000000000) = str(0)
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '0000000000000'
+    
+    @pytest.mark.xfail
+    def test_validate_phone_9(self):
+        p = PhoneField.objects.create(phone="9")
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '999999999'
+
+    @pytest.mark.xfail
+    def test_validate_phone_10(self):
+        p = PhoneField.objects.create(phone="0")
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '999999999'
+    
+    @pytest.mark.xfail
+    def test_validate_phone_11(self):
+        p = PhoneField.objects.create(phone=99999999999999)
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '99999999999999'
+
+    @pytest.mark.xfail
+    def test_validate_phone_12(self):
+        p = PhoneField.objects.create(phone=00000000000000)
+        p.full_clean()
+        p.save()
+        p.refresh_from_db()
+        p = PhoneField.objects.all()
+        assert p[0].phone == '00000000000000'
--- utils/models.py
+++ utils/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.utils.translation import ugettext_lazy as _
 
 
 class PhoneField(models.Model):
-    phone = models.CharField(validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', 
-                                                        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")], 
-                                                        max_length=15, blank=True)
+    phone = models.CharField(
+                max_length=13,
+                blank=False,
+                validators=[
+                    RegexValidator(
+                        regex=r'^\d{9,13}$',
+                        message=_("Phone number must be between 9 and 13 digits")
+                        )
+                    ]
+                )
