da3810d26b98a429bb7748692e3da696c84dcc21
rodrigo-xavier <rodrigomatiasxavier@gmail.com>
2020-06-23 16:16:14 -0300

    ajust profile form

--- cv/models.py
+++ cv/models.py
@@ -5,7 +5,20 @@ from django.utils.translation import ugettext_lazy as _
 from django.core.validators import RegexValidator
 
 class Profile(models.Model):
+    LANGUAGES = [
+        (0, _('None')),
+        (1, _('English')),
+        (2, _('Chinese')),
+        (3, _('Portuguese')),
+    ]
+    FLUENCY = [
+        (1, _('Basic')),
+        (2, _('Intermediary')),
+        (3, _('Advanced')),
+    ]
+
     name = models.CharField(verbose_name=_('Name'), max_length=30)
+    born = models.DateField(verbose_name=_("Born"), unique=True)
     mail = models.EmailField(verbose_name=_("Mail"), max_length=255, unique=True)
     phone = models.CharField(
                 verbose_name=_('Phone'), max_length=13, unique=True,
@@ -13,12 +26,12 @@ class Profile(models.Model):
                     regex=r'^\d{9,13}$',
                     message=_("Phone number must be between 9 and 13 digits")
                     )])
-    about = models.TextField(verbose_name=_("About"), max_length=3000)
+    languages = models.CharField(choices=LANGUAGES, verbose_name=_("Languages"), max_length=30) # Have to be a dict
+    # fluency = models.ChoiceField(choices=[FLUENCY], verbose_name=_("Fluency"))
+    link = models.URLField(verbose_name=_("Other Platform"), max_length=100) # Have to be a dict
+    about = models.TextField(verbose_name=_("About"), max_length=2000)
     current_goals = models.TextField(verbose_name=_("Current Goals"), max_length=1000) # Objetivos atuais
     proffessional_description = models.TextField(verbose_name=_("Professional Description"), max_length=1000) # Descricao profissional (Pode nao ser necessario)
-    link = models.URLField(verbose_name=_("Other Platform"), max_length=100) # Have to be a dict
-    born = models.DateField(verbose_name=_("Born"), unique=True)
-    fluency = models.CharField(verbose_name=_("Fluency"), max_length=30) # Have to be a dict
 
     def __str__(self):
         return self.name
