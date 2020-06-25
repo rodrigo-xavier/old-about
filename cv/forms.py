026f1b64cb942533a54d02210bba06015141c333
rodrigo-xavier <rodrigomatiasxavier@gmail.com>
2020-06-25 10:21:21 -0300

    adding mdb

---
+++ cv/forms.py
@@ -0,0 +1,14 @@
+from cv import models
+from django.forms import ModelForm
+from django.utils.translation import ugettext_lazy as _
+
+class ProfileForm(ModelForm):
+
+    # def __init__(self, *args, **kwargs):
+    #     pass
+
+    class Meta:
+        model = models.Profile
+        fields = ['name', 'born', 'mail', 'mail', 'phone', 'languages', 'links', 
+            'about', 'current_goals', 'proffessional_description'
+        ]
